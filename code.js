2c29c4e30a1329b8b91a84b66ad645b6d71a36f3
Nikodem <wolninikodem@gmail.com>
2024-02-17 15:38:33 +0100

    dodanie local storage i usuwanie

--- code.js
+++ code.js
@@ -1,29 +1,8 @@
-// const form = document.querySelector('.submit-tag')
-// const input1 = document.querySelector('.tekst1')
-// const input2 = document.querySelector('.tekst2')
-
-// function onSubmit(e){
-//     e.preventDefault()
-//     if(input1.value === '' || input2.value === ''){
-//         alert('Please fill all the gaps')
-//         return
-//     }
-//     else{
-//         document.body.style.backgroundColor=input1.value
-//         input1.value = ""
-//         input2.value = ''
-//         console.log(input1.value);
-//     }
-//     console.log('submit')
-// }
-
-// form.addEventListener('submit', onSubmit)
-
 const lista = document.querySelector('#glownatablica')
 const dodaj = document.querySelector('.przycisk')
 const rzeczDoKupienia = document.querySelector('.tekst1')
-const filter = document.querySelector('.filter')
-const clear = document.querySelector('.clear')
+const filter = document.querySelector('#filter')
+const clear = document.querySelector('#clear')
 const form = document.querySelector('form')
 
 function czyjestwliscie(slowo) {
@@ -70,10 +49,23 @@ function ilex(slowo){
 }
 
 function DeleteParent(e){
-    e.target.parentNode.remove()
-    if(lista.innerText==''){
-        lista.className=""
-    }
+    RemoveParent(e.target.parentNode)
+}
+
+function RemoveParent(item){
+    item.remove()
+
+    removeItemFromStorage(item)
+    sprawdzaj_czy_pusta_lista()
+
+}
+function removeItemFromStorage(item){
+    let itemsFromStorage = getItemsFromStorage()
+    const tablica = [item.querySelector('li').textContent, item.querySelector('h5').innerText]
+    itemsFromStorage = itemsFromStorage.filter((i)=>i[0].trim()!==tablica[0].trim())
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+    filter.className='zniknij'
+    clear.className='zniknij'
 }
 
 function OnSubmit(e){
@@ -82,39 +74,14 @@ function OnSubmit(e){
 
         const ile = ilex(rzeczDoKupienia.value.toUpperCase())
         if(ile>0){
-            if(czyjestwliscie(rzeczDoKupienia.value.toUpperCase())===false){
-                ///tworze tablice
-                const newTablica = document.createElement('ul')
-                newTablica.className='tablicaWtablicy'
-                // 
-                /// tworze ilosc
-                const newile = document.createElement('h5')
-                const napisile= document.createTextNode(ile)
-                newile.appendChild(napisile)
-                /// tworze nazwe
-                lista.className='pojawsie'
-                const newItem = document.createElement('li')
-                const napis = document.createTextNode(rzeczDoKupienia.value.toUpperCase())
-                newItem.appendChild(napis)
-                /// tworze znak usun
-                const x = document.createElement('button')
-                x.className='x'
-                const napisdox=document.createTextNode('-')
-                x.appendChild(napisdox)
-                x.addEventListener('click', DeleteParent)
-                /// dodaje wszystko do tablicy
-                newTablica.appendChild(x)
-                newTablica.appendChild(newItem)
-                newTablica.appendChild(newile)
-                /// dodaje tablcie do glownej tablicy
-                lista.appendChild(newTablica)
-                rzeczDoKupienia.value=""
-            } else{
-                const index = czyjestwliscie(rzeczDoKupienia.value.toUpperCase())
-                const ktore = lista.querySelector(`ul:nth-child(${index})`)
-                ktore.querySelector('h5').innerText=Number(ktore.querySelector('h5').innerText)+ile
+            clear.className=''
+            filter.className=''
 
-            }
+            addToStorage([rzeczDoKupienia.value.toUpperCase(), ile])
+            
+            dodajDoDOM(rzeczDoKupienia.value.toUpperCase(), ile)
+
+
         } else{
             alert('Nie dodajesz żadnego produktu')
         }
@@ -124,10 +91,107 @@ function OnSubmit(e){
 }
 
 function OnClick(){
+    localStorage.clear()
     lista.innerText=""
     lista.className="jebackala"
+    filter.className='zniknij'
+    clear.className='zniknij'
 }
 
+function OnAnyKey(e){
+    const items = lista.querySelectorAll('ul')
+    const text = e.target.value.toUpperCase()
+    // const secondItems= items.querySelectorAll('li')
+    let secondItems=[]
+    for(let i=0; i<items.length;i++){
+        secondItems.push(items[i].querySelector('li'))
+    }
+    secondItems.forEach(item=>{
+        const itemName = item.firstChild.textContent
+        if(itemName.indexOf(text)!=-1){
+            item.parentNode.style.display='flex'
+            console.log(true);
+        } else{
+            item.parentNode.style.display='none'
+        }
+    })
+
+}
+
+function dodajDoDOM(co_kupuje, ile){
+
+    if(czyjestwliscie(co_kupuje)===false){
+        ///tworze tablice
+        const newTablica = document.createElement('ul')
+        newTablica.className='tablicaWtablicy'
+        // 
+        /// tworze ilosc
+        const newile = document.createElement('h5')
+        const napisile= document.createTextNode(ile)
+        newile.appendChild(napisile)
+        /// tworze nazwe
+        lista.className='pojawsie'
+        const newItem = document.createElement('li')
+        const napis = document.createTextNode(co_kupuje)
+        newItem.appendChild(napis)
+        /// tworze znak usun
+        const x = document.createElement('button')
+        x.className='x'
+        const napisdox=document.createTextNode('-')
+        x.appendChild(napisdox)
+        x.addEventListener('click', DeleteParent)
+        /// dodaje wszystko do tablicy
+        newTablica.appendChild(x)
+        newTablica.appendChild(newItem)
+        newTablica.appendChild(newile)
+        /// dodaje tablcie do glownej tablicy
+        lista.appendChild(newTablica)
+        rzeczDoKupienia.value=""
+    } else{
+        const index = czyjestwliscie(co_kupuje)
+        const ktore = lista.querySelector(`ul:nth-child(${index})`)
+        ktore.querySelector('h5').innerText=Number(ktore.querySelector('h5').innerText)+ile
+
+    }
+}
+
+function addToStorage(item){
+    let itemsFromStorage = getItemsFromStorage()
+    itemsFromStorage.push(item)
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+function getItemsFromStorage(){
+    let itemsFromStorage
+    if(localStorage.getItem('items')===null){
+        itemsFromStorage=[]
+    } else{
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+
+    }
+
+    return itemsFromStorage
+
+}
+
+function displayItems(){
+    const itemsFromStorage = getItemsFromStorage()
+    itemsFromStorage.forEach(item=>dodajDoDOM(item[0], item[1]))
+
+}
+
+function sprawdzaj_czy_pusta_lista(){
+    if(lista.innerText===''){
+        lista.className='zniknij'
+        lista.className=''
+    } else{
+        clear.className=''
+        filter.className=''
+    }
+}
 
 clear.addEventListener('click', OnClick)
 form.addEventListener('submit', OnSubmit)
+filter.addEventListener('input', OnAnyKey)
+document.addEventListener('DOMContentLoaded', displayItems)
+document.addEventListener('DOMContentLoaded',sprawdzaj_czy_pusta_lista)
